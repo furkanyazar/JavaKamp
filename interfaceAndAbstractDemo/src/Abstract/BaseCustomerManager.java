6160683cbc8e472f1b42e200e47935d8f37b85b5
Furkan Yazar <frknyz1@gmail.com>
2021-05-03 18:03:46 +0300

    Ufak düzeltmeler

--- interfaceAndAbstractDemo/src/Abstract/BaseCustomerManager.java
+++ interfaceAndAbstractDemo/src/Abstract/BaseCustomerManager.java
@@ -6,7 +6,7 @@ public abstract class BaseCustomerManager implements ICustomerService {
 
 	@Override
 	public void Save(Customer customer) throws Exception {
-		System.out.println("Saved to db: " + customer.getFirstName());
+		System.out.println("Saved to database: " + customer.getFirstName());
 	}
 
 }
--- interfaceAndAbstractDemo/src/Adapters/MernisServiceAdapter.java
+++ interfaceAndAbstractDemo/src/Adapters/MernisServiceAdapter.java
@@ -11,13 +11,9 @@ public class MernisServiceAdapter implements ICustomerCheckService {
 	@Override
 	public boolean CheckIfRealPerson(Customer customer) throws NumberFormatException, RemoteException {
 		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
-		
-		return kpsPublicSoapProxy.TCKimlikNoDogrula(
-				Long.parseLong(customer.getNationalityId()),
-				customer.getFirstName(),
-				customer.getLastName(),
-				customer.getDateOfBirth().getYear()
-				);
+
+		return kpsPublicSoapProxy.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()),
+				customer.getFirstName(), customer.getLastName(), customer.getDateOfBirth().getYear());
 	}
 
 }
